64392d1a900904d0ebb4199eac116b2d5d7f27fa
sebaspv <poncegramming@gmail.com>
2022-01-15 18:11:28 -0600

    added support for specialist recommendation

---
+++ README.md
@@ -0,0 +1,5 @@
+# Disease detection API for MediSearch
+An API that uses NLP and AI to let you predict possible diseases and symptoms based on a prompt of what you're feeling.
+
+## Available routes
+This app only has one route `/get_illness` that lets you enter a prompt of how you're feeling, your age and sex and get possible diseases and symptoms. You can see it in action in the `/docs` route.
--- symptoms_api/main.py
+++ symptoms_api/main.py
@@ -31,17 +31,24 @@ async def get_symptoms(prompt: str, sex: str, age: int) -> dict:
         symptom.pop("type")
         symptom.pop("orth")
     data_illness = {"sex": sex, "age": {"value": age}, "evidence": symptoms_content}
-    possible_illnesses = requests.post(
+    possible_illnesses_request = requests.post(
         config["API_URL"] + "diagnosis", headers=headers, data=json.dumps(data_illness)
     )
+    possible_specialist = requests.post(
+        config["API_URL"] + "recommend_specialist", headers=headers, data=json.dumps(data_illness)
+    )
+    specialist = json.loads(possible_specialist.text)["recommended_specialist"]
     possible_symptoms_content = json.loads(symptoms.text)["mentions"]
-    possible_illnesses = json.loads(possible_illnesses.text)["conditions"]
+    possible_illnesses = json.loads(possible_illnesses_request.text)["conditions"]
     possible_symptoms = possible_symptoms_content
     for illness in possible_illnesses:
         illness["description"] = wiki.page(illness["name"]).summary.split(".")[0]
         if illness["description"] == "":
             illness["description"] = "No info available"
-    return {"illnesses": possible_illnesses, "symptoms": possible_symptoms}
+    specialist["description"] = wiki.page(specialist["name"]).summary.split(".")[0]
+    if specialist["description"] == "":
+        specialist["description"] == "No info available"
+    return {"illnesses": possible_illnesses, "symptoms": possible_symptoms, "emergency": json.loads(possible_illnesses_request.text)["has_emergency_evidence"], "specialist": specialist}
 
 
 if __name__ == "__main__":
